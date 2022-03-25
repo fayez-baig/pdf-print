c2bf77fcf9c330a48222c257bbda7b5e04a6c27e
fayez-baig <coderninja16@gmail.com>
2022-03-26 01:55:27 +0530

    part 1

--- src/summary/summaryPage.js
+++ src/summary/summaryPage.js
@@ -16,6 +16,7 @@ export const PDFPage = styled.div`
       margin: 10px 0;
       size: A4 portrait;
     }
+
     /* page-break-after: always; */
   }
 `;
@@ -44,26 +45,44 @@ export const StyledHeaderColumnCenter = styled.div`
 export const StyledHeaderColumnRight = styled.div``;
 
 export const StyledBox = styled.div`
-  width: 50%;
-  display: flex;
-  min-width: 50%;
-  /* border: 1px solid #b7bccf; */
-  padding: 1rem;
-  margin-top: 3rem;
-  float: left;
-`;
+  columns: 2 280px;
+  column-gap: 1rem;
+  background: red;
+  color: white;
+  margin: 0 0 1rem;
+  display: inline-block; /* stops block being split across columns. Could use break-inside: avoid for modern browsers; */
+  width: 100%;
+  break-inside: avoid;
+  text-align: center;
+  font-weight: bold;
 
-export const Wrapper = styled.div`
-  float: left;
   @media print {
     break-inside: avoid;
     -webkit-break-inside: avoid;
   }
 `;
 
+export const ContentBox = styled.div`
+  float: left;
+  width: 50%;
+  height: 100%;
+  &:after {
+    display: table;
+    content: "";
+    clear: both;
+  }
+`;
+
+export const Wrapper = styled.div`
+  columns: 2 280px;
+  column-gap: 1rem;
+  font-size: 1.2rem;
+`;
+
 export const StyledImage = styled.img`
   height: 200px;
   width: 200px;
+  height: 100%;
   margin-right: 2rem;
   @media print {
     height: 100px;
